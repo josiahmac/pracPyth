5d47323afd4fe7ee8e2b22a6ab89f67604c00648
josiah.macabante <josiah.macabante@pointwest.com.ph>
2024-10-02 10:25:04 +0800

    rawr

--- pyth.py
+++ pyth.py
@@ -114,32 +114,81 @@ import time, sys
 # # print(spam(0))
 # print(spam(1))
 
-def zigZag():
-    indent = 0
-    indentIncreasing = True
+# def zigZag():
+#     indent = 0
+#     indentIncreasing = True
 
-    try:
-        while True:
+#     try:
+#         while True:
             
-                print(' '*indent, end='')
-                print('**********')
+#                 print(' '*indent, end='')
+#                 print('**********')
 
-                time.sleep(0.1) 
+#                 time.sleep(0.1) 
 
-                if indentIncreasing:
-                    indent+=random.randint(1,3)
+#                 if indentIncreasing:
+#                     indent+=random.randint(1,3)
 
-                    if indent>=random.randint(5,25):
-                        indentIncreasing = False
+#                     if indent>=random.randint(5,25):
+#                         indentIncreasing = False
                 
-                else:
-                    indent-=random.randint(1,3)
-                    if indent<=0:
-                        indentIncreasing = True
-    except KeyboardInterrupt:
-        sys.exit
+#                 else:
+#                     indent-=random.randint(1,3)
+#                     if indent<=0:
+#                         indentIncreasing = True
+#     except KeyboardInterrupt:
+#         sys.exit
 
 
-zigZag()
+# zigZag()
 
+
+listahan = ['rawr', 'rar', 'rur', 'ror', 'rauwr']
+
+# listatu= listahan[1:4]*3+listahan
+
+# print(listatu)
+
+# for i in listahan:
+#     print(i)
+
+# print('dlalja' in listahan)
+
+# for index, item in enumerate(listahan): *****
+#     print(str(index)+' = '+item)
+
+# print(random.choice(listahan))
+
+# random.shuffle(listahan)
+
+# for i in listahan:
+#     print(i)
+
+# for i in random.choice(listahan):
+#     print(i)
+# print(listahan.index('ror'))
+
+listahan.append('hkhk')
+listahan.insert(0,'kokokok')
+
+# for i in listahan:
+#     print(i)
+
+# listahan.remove('hkhk')
+
+# for i in listahan:
+#     print(i)
+
+# listahan.sort()
+
+# for i in listahan:
+#     print(i)
+
+# listahan.sort(reverse=True)
+
+# for i in listahan:
+#     print(i)
+
+# listahan.sort(key=str.upper)
+# print(listahan)
 
